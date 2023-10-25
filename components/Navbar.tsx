15cf57391ce028f818dd541c3c16b9d04da594ec
Alessandro Degenkamp <alessandrodegenkamp@hotmail.com>
2023-10-25 11:28:48 +0200

    changes to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='border-2 flexBetween max-container padding-container relative z-30 py-5'>
+        <Link href="/">
+            <Image src='/hilink-logo.svg' alt='logo' width={74} height={29}/>
+        </Link>
+    </nav>
   )
 }
