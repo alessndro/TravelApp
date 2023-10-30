1d6b3c0f28daf08bd4b0a8c00dea22e52bacb72d
Alessandro Degenkamp <alessandrodegenkamp@hotmail.com>
2023-10-30 14:22:29 +0100

    changed button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
 
-export default function Button(props) {
+type ButtonProps = {
+  theme: string; // Specify the appropriate type for theme
+  icon?: string; // Make icon an optional string prop
+  title: string;
+};
+
+export default function Button(props: ButtonProps) {
+  
   return (
     <button className={`${props.theme} flexCenter gap-2 rounded-full border`}>
         {props.icon && <Image src={props.icon} width={15} height={15} alt="small user icon" />}
