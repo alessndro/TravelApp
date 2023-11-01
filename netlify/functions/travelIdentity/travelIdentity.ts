312b5322ce6c452db42a783ceda7479e58166c8a
Alessandro Degenkamp <alessandrodegenkamp@hotmail.com>
2023-11-01 09:37:53 +0100

    work around in severless function

--- netlify/functions/travelIdentity/travelIdentity.ts
+++ netlify/functions/travelIdentity/travelIdentity.ts
@@ -1,8 +1,10 @@
 import { Handler } from '@netlify/functions'
 
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters || {}
+  // const { name = 'stranger' } = event.queryStringParameters || {}
 
+  const queryStringParameters = event.queryStringParameters || {};
+  const name = queryStringParameters.name || 'stranger';
   return {
     statusCode: 200,
     body: JSON.stringify({
