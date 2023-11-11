502c95e1a2191094f36bed9bc86e0d2f75777e1f
Alessandro Degenkamp <alessandrodegenkamp@hotmail.com>
2023-11-11 14:22:41 +0100

    added loading svg

--- components/TravelPlan.tsx
+++ components/TravelPlan.tsx
@@ -49,6 +49,7 @@ export default function travelPlan() {
     async function handleSubmit(event: React.FormEvent<HTMLFormElement>){
         event.preventDefault();
         try {
+            setLoading(true)
             const response = await fetch('https://gotravelapp.netlify.app/.netlify/functions/travelPlan', {
                 method: 'POST',
                 headers: {
@@ -73,6 +74,7 @@ export default function travelPlan() {
             console.log(error)
         }
         finally {
+            setLoading(false)
             console.log(generatedPlan)
         }
     }
@@ -83,8 +85,8 @@ return (
     
         
         {/* LEFT SIDE OF HERO */}
-      { !loading ? <div className=' max-container padding-container py-10 2xl:py-20 gap-20 flex flex-col  md:flex-row '>
-        
+       <div className=' max-container padding-container py-10 2xl:py-20 gap-20 flex flex-col  md:flex-row '>
+    
         {!generatedPlan.title ? <div className='relative z-20 bg-white flex flex-1 flex-col shadow-2xl rounded-3xl p-5 md:w-1/2'>
             <Image 
                 src="/plane.svg"
@@ -96,6 +98,14 @@ return (
              <h2 className='bold-32 lg:bold-52'>Let's Craft Your Journey! </h2>
              <p className='regular-16 text-gray-30 mt-2'> Share your travel details, and we'll shape a one-of-a-kind journey tailored to your interests. Get started now and embark on a unique exploration. </p>
             <div className='my-3'>
+                {loading ? <div className='flex w-full h-full items-center justify-center'><Image 
+                src="/loadingTravel.svg"
+                alt='loading icon'
+                width={50}
+                height={50}
+                className=''
+            /></div> :
+                <>
                 <h3 className='mb-2'>Choose your personality</h3>
                 <div className='flex flex-row overflow-y-hidden overflow-x-auto gap-1 '>
                     <div onClick={() => handleChooseIdentity('Culture and City Explorer')} className={`${travelDetails.identity === 'Culture and City Explorer' ? ' border-black' : ''} border flex flex-col items-center flex-shrink-0 rounded-2xl shadow-xl`}>
@@ -119,6 +129,8 @@ return (
                         <p className='regular-14 -mt-10 mb-3'>Budget Traveler</p>
                     </div>
                 </div>
+                </>
+                }
             </div>
             <div className='max-w-lg'>
                 <form onSubmit={handleSubmit}>
@@ -198,6 +210,8 @@ return (
                 </div>
             </div>
         </div> }
+
+        }
         <div className='relative z-20  flex flex-1 flex-col  justify-center items-start md:w-1/2'>
 
             <div className='hidden absolute top-0 md:flex bg-green-90 rounded-3xl px-5 py-5'>
@@ -223,7 +237,7 @@ return (
             
         </div>
         </div>
-       : <h1>loading</h1>}
+       
     </section>
   );
 }
