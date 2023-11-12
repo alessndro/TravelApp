8496f89f78e90cd452064c234cb35cd74ed74f6f
Alessandro Degenkamp <alessandrodegenkamp@hotmail.com>
2023-11-12 21:15:17 +0100

    changed features page

--- components/Features.tsx
+++ components/Features.tsx
@@ -27,60 +27,36 @@ export default function Features() {
              <div className='flex flex-col lg:grid lg:grid-cols-2 lg:gap-10'>
                 
                 <div className='flex flex-col mt-5'>
-                    <div className='bg-green-500 rounded-full p-3 w-[50px] items-start'>
-                        <Image 
-                            src="/map.svg"
-                            alt='camp icon'
-                            width={30}
-                            height={30}
-                            className=''
-                        />
+                    <div className='bg-green-500 rounded-full p-3 w-[50px] items-start text-center'>
+                        <p className='text-white bold-20'>1</p>
                     </div>
                     <div className='bg-white-100'>
-                    <h3 className='bold-20 lg:bold-32 lg:mt-5'>Travel Personality Survey</h3>
+                    <h3 className='bold-20 lg:bold-32 lg:mt-5'>Take the Personality Survey</h3>
                         <p className='regular-14 text-gray-30 mt-2'>Discover your unique travel identity through our comprehensive Travel Personality Survey. Answer thought-provoking questions about your travel preferences, interests, and style to uncover the adventurer within you.</p>
                     </div>
                 </div>
 
                 <div className='flex flex-col mt-5'>
-                <div className='bg-green-500 rounded-full p-3 w-[50px] items-start'>
-                        <Image 
-                            src="/map.svg"
-                            alt='camp icon'
-                            width={30}
-                            height={30}
-                            className='flex-1'
-                        />
+                    <div className='bg-green-500 rounded-full p-3 w-[50px] items-start text-center'>
+                        <p className='text-white bold-20'>2</p>
                     </div>
-                    <h3 className='bold-20 lg:bold-32 lg:mt-5'>Cutting-Edge AI Technology</h3>
-                    <p className='regular-14 text-gray-30 mt-2'>We leverage state-of-the-art AI technology to create personalized travel itineraries tailored to your individuality. Our AI engine analyzes your survey responses and user profile to generate custom travel plans that perfectly align with your preferences.</p>
+                    <h3 className='bold-20 lg:bold-32 lg:mt-5'>Discover Your Travel Personality</h3>
+                    <p className='regular-14 text-gray-30 mt-2'>Once you've completed the survey, the website will analyze your responses and categorize you into a specific travel personality type. Whether you're an Adventure Seeker, Culture Explorer, Relaxation Enthusiast, or Urban Adventurer, your travel personality will serve as the foundation for creating a tailor-made experience just for you.</p>
                 </div>
 
                 <div className='flex flex-col mt-5'>
-                <div className='bg-green-500 rounded-full p-3 w-[50px] items-start'>
-                        <Image 
-                            src="/map.svg"
-                            alt='camp icon'
-                            width={30}
-                            height={30}
-                            className='flex-1'
-                        />
+                    <div className='bg-green-500 rounded-full p-3 w-[50px] items-start text-center'>
+                        <p className='text-white bold-20'>3</p>
                     </div>
-                    <h3 className='bold-20 lg:bold-32 lg:mt-5'>Location and Vacation Customization</h3>
+                    <h3 className='bold-20 lg:bold-32 lg:mt-5'>Customize Your Travel Plan</h3>
                     <p className='regular-14 text-gray-30 mt-2'>Your adventure starts with you. Share your starting location and vacation preferences, and watch as we customize your journey to match your desires. Whether you're looking for a serene escape or an action-packed adventure, we've got you covered</p>
                 </div>
 
                 <div className='flex flex-col mt-5'>
-                    <div className='bg-green-500 rounded-full p-3 w-[50px] items-start'>
-                        <Image 
-                            src="/map.svg"
-                            alt='camp icon'
-                            width={30}
-                            height={30}
-                            className='flex-1'
-                        />
+                    <div className='bg-green-500 rounded-full p-3 w-[50px] items-start text-center'>
+                        <p className='text-white bold-20'>4</p>
                     </div>
-                    <h3 className='bold-20 lg:bold-32 lg:mt-5'>Activities Tailored to Your Identity</h3>
+                    <h3 className='bold-20 lg:bold-32 lg:mt-5'>Receive Your Personalized Itinerary</h3>
                     <p className='regular-14 text-gray-30 mt-2'>We handpick activities and experiences that resonate with your unique travel personality. From cultural explorations and outdoor adventures to culinary delights, your itinerary is a reflection of who you are and what you love.</p>
                 </div>
 
